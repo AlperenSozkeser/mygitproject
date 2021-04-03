9b5da4418bd15b3d422a49e103d4df83b0550739
Alperen <alperencemalsozkeser@gmail.com>
2021-04-03 18:50:50 +0200

    version 3

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,7 +5,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("My second commit");
-		
+		System.out.println("alperen Cemal");
 	}
 
 }
